729247bbf649ea29e597c6432f92f346bdb497ae
tomcat99999 <cerveny.t@seznam.cz>
2016-05-23 12:34:35 +0200

    druhy pokus

---
+++ src/main/java/javappj/ComentDao.java
@@ -0,0 +1,46 @@
+package javappj;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Created by The CAT on 23.5.2016.
+ */
+@Transactional
+public class ComentDao {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public Session session() {
+        return sessionFactory.getCurrentSession();
+    }
+    @SuppressWarnings("unchecked")
+
+
+    public List<Coment> getComUser(String username) {
+        Criteria crit = session().createCriteria(Coment.class);
+
+        crit.createAlias("coments", "c");
+
+        crit.add(Restrictions.eq("c.enabled", true));
+        crit.add(Restrictions.eq("c.user", username));
+
+        return crit.list();
+    }
+    public List<Coment> getComImg(int imgid) {
+        Criteria crit = session().createCriteria(Coment.class);
+
+        crit.createAlias("coments", "c");
+
+        crit.add(Restrictions.eq("c.enabled", true));
+        crit.add(Restrictions.eq("c.img", imgid));
+
+        return crit.list();
+    }
+}
